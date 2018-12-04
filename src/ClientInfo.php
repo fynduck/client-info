b661f89b2db45259af09fb348bed4e3f47437d7a
fynduck <stasskrim_-92@mail.ru>
2018-12-04 11:30:58 +0200

    updated method detect browser and platform

--- src/ClientInfo.php
+++ src/ClientInfo.php
@@ -16,20 +16,24 @@ trait ClientInfo
         if (!$agent)
             $agent = request()->header('User-Agent');
 
-        if (preg_match('/linux/i', $agent))
-            $platform = 'Linux';
-        elseif (preg_match('/iPhone os x/i', $agent))
-            $platform = 'iPhone';
-        elseif (preg_match('/macintosh|mac os x/i', $agent))
-            $platform = 'Mac';
-        elseif (preg_match('/windows phone/i', $agent))
-            $platform = 'Windows phone';
-        elseif (preg_match('/windows|win32/i', $agent))
-            $platform = 'Windows';
-        elseif (preg_match('/Android/i', $agent))
-            $platform = 'Android';
-        else
-            $platform = 'Unknown';
+        $platform = "Unknown OS Platform";
+
+        $os_array = [
+            '/windows|win32/i'                  => 'Windows',
+            '/mac_powerpc|macintosh|mac os x/i' => 'Mac',
+            '/linux/i'                          => 'Linux',
+            '/ubuntu/i'                         => 'Ubuntu',
+            '/iphone/i'                         => 'iPhone',
+            '/ipod/i'                           => 'iPod',
+            '/ipad/i'                           => 'iPad',
+            '/android/i'                        => 'Android',
+            '/blackberry/i'                     => 'BlackBerry',
+            '/webos/i'                          => 'Mobile'
+        ];
+
+        foreach ($os_array as $regex => $value)
+            if (preg_match($regex, $agent))
+                $platform = $value;
 
         return $platform;
     }
@@ -39,44 +43,31 @@ trait ClientInfo
         if (!$agent)
             $agent = request()->header('User-Agent');
 
-        $data = ['browserName' => '', 'up' => ''];
-        if (preg_match('/MSIE/i', $agent) && !preg_match('/Opera/i', $agent)) {
-            $data['browserName'] = 'Internet Explorer';
-            $data['ub'] = "MSIE";
-        } elseif (preg_match('/Firefox/i', $agent)) {
-            $data['browserName'] = 'Mozilla Firefox';
-            $data['ub'] = "Firefox";
-        } elseif (preg_match('/Opera/i', $agent)) {
-            $data['browserName'] = 'Opera';
-            $data['ub'] = "Opera";
-        } elseif (preg_match('/Netscape/i', $agent)) {
-            $data['browserName'] = 'Netscape';
-            $data['ub'] = "Netscape";
-        } elseif (strpos($agent, "Edge") !== false) {
-            $data['browserName'] = 'Microsoft Edge';
-            $data['ub'] = "Edge";
-        } elseif (preg_match('/Chrome/i', $agent)) {
-            $data['browserName'] = 'Google Chrome';
-            $data['ub'] = "Chrome";
-        } elseif (preg_match('/Safari/i', $agent)) {
-            $data['browserName'] = 'Apple Safari';
-            $data['ub'] = "Safari";
-        } elseif (preg_match('/YaBrowser/i', $agent)) {
-            $data['browserName'] = 'Yandex browser';
-            $data['ub'] = "YaBrowser";
-        } elseif (strpos($agent, "facebook") !== false) {
-            $data['browserName'] = 'Facebook';
-            $data['ub'] = "Facebook Bot";
-        } elseif (strpos($agent, "Sitemaps") !== false) {
-            $data['browserName'] = 'Sitemap';
-            $data['ub'] = "Sitemap Bot";
-        } elseif (strpos($agent, "bot") !== false) {
-            $data['browserName'] = 'Bots';
-            $data['ub'] = "Other Bots";
-        } else {
-            $data['ub'] = "Bot";
-            $data['browserName'] = "";
-        }
+        $data = ['browserName' => 'Unknown Browser', 'ub' => ''];
+
+        $browser_array = [
+            '/msie/i'      => 'Internet Explorer',
+            '/firefox/i'   => 'Firefox',
+            '/safari/i'    => 'Safari',
+            '/chrome/i'    => 'Chrome',
+            '/edge/i'      => 'Edge',
+            '/opera/i'     => 'Opera',
+            '/netscape/i'  => 'Netscape',
+            '/maxthon/i'   => 'Maxthon',
+            '/konqueror/i' => 'Konqueror',
+            '/mobile/i'    => 'Handheld Browser',
+            '/YaBrowser/i' => 'Yandex browser',
+        ];
+
+        foreach ($browser_array as $regex => $value) {
+            if (preg_match($regex, $agent)) {
+                $data['browserName'] = $value;
+                $data['ub'] = str_replace(['/i', '/'], '', $regex);
+            } elseif (strpos($agent, 'bot') !== false) {
+                $data['browserName'] = 'Bots';
+                $data['ub'] = 'Bot';
+            }
+        };
 
         return $data;
     }
@@ -104,7 +95,7 @@ trait ClientInfo
             //we will have two since we are not using 'other' argument yet
             //see if version is before or after the name
             if (strripos($agent, "Version") < strripos($agent, $ub)) {
-                $data['version'] = $matches['version'][0];
+                $data['version'] = isset($matches['version']) ? $matches['version'][0] : 0;
             } else {
                 if (!empty($matches['version'])) {
                     $data['version'] = $matches['version'][1];
